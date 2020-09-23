94f39b1063fc3061e7ef67340c33fcd79b65918c
zjun <565061763@qq.com>
2020-09-23 23:14:07 +0800

    test

--- app/src/androidTest/java/com/sd/collection_map/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sd/collection_map/ExampleInstrumentedTest.java
@@ -2,13 +2,15 @@ package com.sd.collection_map;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.sd.lib.collection.map.impl.FUniqueMap;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -24,5 +26,20 @@ public class ExampleInstrumentedTest
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.sd.collection_map", appContext.getPackageName());
+    }
+
+    @Test
+    public void test_FUniqueMap()
+    {
+        final FUniqueMap<String, String> map = new FUniqueMap<>();
+        map.put("a", "1");
+        map.put("b", "1");
+        map.put("c", "1");
+        map.put("c", "1");
+
+        assertEquals(1, map.size());
+        assertEquals("1", map.get("c"));
+        assertEquals("c", map.getKeyByValue("1"));
+        assertEquals("c", map.removeKeyByValue("1"));
     }
 }
