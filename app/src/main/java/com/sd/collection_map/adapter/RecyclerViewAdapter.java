d752c2f8072e08b993db7e2137a86f63dc1c8fa0
zjun <565061763@qq.com>
2020-09-24 22:31:10 +0800

    demo

--- app/src/main/java/com/sd/collection_map/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/sd/collection_map/adapter/RecyclerViewAdapter.java
@@ -1,5 +1,9 @@
 package com.sd.collection_map.adapter;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.sd.collection_map.R;
@@ -7,9 +11,21 @@ import com.sd.collection_map.databinding.ItemRecyclerViewBinding;
 import com.sd.collection_map.model.DataModel;
 import com.sd.lib.adapter.FSimpleRecyclerAdapter;
 import com.sd.lib.adapter.viewholder.FRecyclerViewHolder;
+import com.sd.lib.collection.map.IUniqueMap;
+import com.sd.lib.collection.map.impl.FUniqueMap;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 public class RecyclerViewAdapter extends FSimpleRecyclerAdapter<DataModel>
 {
+    private static final String TAG = RecyclerViewAdapter.class.getSimpleName();
+    private static final Handler HANDLER = new Handler(Looper.getMainLooper());
+
+    private final Map<View, Integer> mMapWeak = new WeakHashMap<>();
+    private final IUniqueMap<Integer, View> mMapPositionView = new FUniqueMap<>();
+
     @Override
     public int getLayoutId(ViewGroup parent, int viewType)
     {
@@ -21,5 +37,28 @@ public class RecyclerViewAdapter extends FSimpleRecyclerAdapter<DataModel>
     {
         final ItemRecyclerViewBinding binding = ItemRecyclerViewBinding.bind(holder.itemView);
         binding.btn.setText(model.mObject.toString());
+
+        mMapWeak.put(holder.itemView, position);
+        mMapPositionView.put(position, holder.itemView);
+
+        HANDLER.removeCallbacks(mRunnable);
+        HANDLER.postDelayed(mRunnable, 1 * 1000);
+    }
+
+    private final Runnable mRunnable = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            printMapPositionView();
+        }
+    };
+
+    private void printMapPositionView()
+    {
+        final HashSet<Integer> keySet = new HashSet<>(mMapPositionView.toMap().keySet());
+        final HashSet<View> valueSet = new HashSet<>(mMapPositionView.toMap().values());
+
+        Log.i(TAG, "mapSize:" + mMapPositionView.toMap().size() + " keySize:" + keySet.size() + " valueSize:" + valueSet.size());
     }
 }
