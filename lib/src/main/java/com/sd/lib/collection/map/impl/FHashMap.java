a665293e512c378afeb3aa12b9fdfe7700908e2d
zjun <565061763@qq.com>
2020-09-24 09:05:00 +0800

    FHashMap

---
+++ lib/src/main/java/com/sd/lib/collection/map/impl/FHashMap.java
@@ -0,0 +1,65 @@
+package com.sd.lib.collection.map.impl;
+
+import com.sd.lib.collection.map.IMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FHashMap<K, V> implements IMap<K, V>
+{
+    private final Map<K, V> mMap = new HashMap<>();
+
+    @Override
+    public V put(K key, V value)
+    {
+        if (key == null || value == null)
+            return null;
+
+        return mMap.put(key, value);
+    }
+
+    @Override
+    public V remove(Object key)
+    {
+        if (key == null)
+            return null;
+
+        return mMap.remove(key);
+    }
+
+    @Override
+    public V get(Object key)
+    {
+        if (key == null)
+            return null;
+
+        return mMap.get(key);
+    }
+
+    @Override
+    public boolean containsKey(Object key)
+    {
+        if (key == null)
+            return false;
+
+        return mMap.containsKey(key);
+    }
+
+    @Override
+    public int size()
+    {
+        return mMap.size();
+    }
+
+    @Override
+    public void clear()
+    {
+        mMap.clear();
+    }
+
+    @Override
+    public Map<K, V> toMap()
+    {
+        return new HashMap<>(mMap);
+    }
+}
