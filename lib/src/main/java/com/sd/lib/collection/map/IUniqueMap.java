2e4ac39935d589525f9d5e25a0907c08741eeaaf
zjun <565061763@qq.com>
2021-05-08 22:10:39 +0800

    format

--- lib/src/main/java/com/sd/lib/collection/map/IUniqueMap.java
+++ lib/src/main/java/com/sd/lib/collection/map/IUniqueMap.java
@@ -2,25 +2,16 @@ package com.sd.lib.collection.map;
 
 /**
  * key和value都是唯一的Map
- *
- * @param <K>
- * @param <V>
  */
 public interface IUniqueMap<K, V> extends IMap<K, V>
 {
     /**
      * 根据value移除键值对
-     *
-     * @param value
-     * @return
      */
     K removeByValue(Object value);
 
     /**
      * 根据value查找key
-     *
-     * @param value
-     * @return
      */
     K getKeyByValue(Object value);
 }
