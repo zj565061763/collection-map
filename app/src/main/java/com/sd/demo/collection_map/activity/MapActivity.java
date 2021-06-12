846ea51ecf628c07721cc34a9a8c815e4ab5c2fe
zjun <565061763@qq.com>
2021-06-12 14:14:12 +0800

    demo

--- app/src/main/java/com/sd/demo/collection_map/activity/MapActivity.java
+++ app/src/main/java/com/sd/demo/collection_map/activity/MapActivity.java
@@ -9,9 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.sd.demo.collection_map.databinding.ActivityMapBinding;
 import com.sd.lib.collection.map.IMap;
 import com.sd.lib.collection.map.impl.FWeakKeyMap;
-import com.sd.lib.collection.map.impl.FWeakKeyUniqueMap;
 import com.sd.lib.collection.map.impl.FWeakValueMap;
-import com.sd.lib.collection.map.impl.FWeakValueUniqueMap;
 
 public class MapActivity extends AppCompatActivity {
     public static final String TAG = MapActivity.class.getSimpleName();
@@ -21,9 +19,6 @@ public class MapActivity extends AppCompatActivity {
     private final FWeakKeyMap<Object, Object> mWeakKeyMap = new FWeakKeyMap<>();
     private final FWeakValueMap<Object, Object> mWeakValueMap = new FWeakValueMap<>();
 
-    private final FWeakKeyUniqueMap<Object, Object> mWeakKeyUniqueMap = new FWeakKeyUniqueMap<>();
-    private final FWeakValueUniqueMap<Object, Object> mWeakValueUniqueMap = new FWeakValueUniqueMap<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,14 +27,6 @@ public class MapActivity extends AppCompatActivity {
 
         mWeakKeyMap.put(new Object(), this);
         mWeakValueMap.put(this, new Object());
-
-        mWeakKeyUniqueMap.put(new Object(), this);
-        mWeakKeyUniqueMap.put(new Object(), this);
-        mWeakKeyUniqueMap.put(this, new Object());
-
-        mWeakValueUniqueMap.put(this, new Object());
-        mWeakValueUniqueMap.put(this, new Object());
-        mWeakValueUniqueMap.put(new Object(), this);
 
         mBinding.btnTime.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,8 +45,6 @@ public class MapActivity extends AppCompatActivity {
     private void printSize() {
         Log.i(TAG, "FWeakKeyMap size:" + mWeakKeyMap.size());
         Log.i(TAG, "FWeakValueMap size:" + mWeakValueMap.size());
-        Log.i(TAG, "FWeakKeyUniqueMap size:" + mWeakKeyUniqueMap.size());
-        Log.i(TAG, "FWeakValueUniqueMap size:" + mWeakValueUniqueMap.size());
     }
 
     private void printTime(IMap<Object, Object> map) {
