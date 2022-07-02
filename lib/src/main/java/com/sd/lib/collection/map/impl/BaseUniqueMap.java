8da538dbe7fbb3854ba8ad503ee873ce88f70e62
zhengjun <565061763@qq.com>
2022-07-02 22:52:47 +0800

    BaseUniqueMap

--- lib/src/main/java/com/sd/lib/collection/map/impl/BaseUniqueMap.java
+++ lib/src/main/java/com/sd/lib/collection/map/impl/BaseUniqueMap.java
@@ -8,7 +8,7 @@ import java.util.Map;
 /**
  * key和value都是唯一的Map
  */
-public class BaseUniqueMap<K, V> implements IUniqueMap<K, V> {
+abstract class BaseUniqueMap<K, V> implements IUniqueMap<K, V> {
     private final IMap<K, V> mMap;
     private final IMap<V, K> mMapReverse;
 
@@ -19,9 +19,7 @@ public class BaseUniqueMap<K, V> implements IUniqueMap<K, V> {
 
     @Override
     public V put(K key, V value) {
-        if (key == null || value == null) {
-            return null;
-        }
+        if (key == null || value == null) return null;
 
         V resultValue = null;
 
@@ -41,38 +39,27 @@ public class BaseUniqueMap<K, V> implements IUniqueMap<K, V> {
 
     @Override
     public V remove(Object key) {
-        if (key == null) {
-            return null;
-        }
+        if (key == null) return null;
 
         final V value = mMap.remove(key);
-        if (value == null) {
-            return null;
-        }
+        if (value == null) return null;
 
         final K cacheKey = mMapReverse.remove(value);
         if (cacheKey == null) {
             throw new RuntimeException("Cached key was not found");
         }
-
         return value;
     }
 
     @Override
     public V get(Object key) {
-        if (key == null) {
-            return null;
-        }
-
+        if (key == null) return null;
         return mMap.get(key);
     }
 
     @Override
     public boolean containsKey(Object key) {
-        if (key == null) {
-            return false;
-        }
-
+        if (key == null) return false;
         return mMap.containsKey(key);
     }
 
@@ -94,29 +81,21 @@ public class BaseUniqueMap<K, V> implements IUniqueMap<K, V> {
 
     @Override
     public K removeByValue(Object value) {
-        if (value == null) {
-            return null;
-        }
+        if (value == null) return null;
 
         final K key = mMapReverse.remove(value);
-        if (key == null) {
-            return null;
-        }
+        if (key == null) return null;
 
         final V cacheValue = mMap.remove(key);
         if (cacheValue == null) {
             throw new RuntimeException("Cached value was not found");
         }
-
         return key;
     }
 
     @Override
     public K getKeyByValue(Object value) {
-        if (value == null) {
-            return null;
-        }
-
+        if (value == null) return null;
         return mMapReverse.get(value);
     }
 }
