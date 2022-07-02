859e41f882a20568f007b11a5fd682b0ea8947b2
zhengjun <565061763@qq.com>
2022-07-02 23:52:51 +0800

    FUniqueMap

--- lib/src/main/java/com/sd/lib/collection/map/impl/FWeakValueUniqueMap.java
+++ lib/src/main/java/com/sd/lib/collection/map/impl/FWeakValueUniqueMap.java
@@ -3,7 +3,7 @@ package com.sd.lib.collection.map.impl;
 /**
  * value是弱引用，并且key和value都是唯一的Map
  */
-public class FWeakValueUniqueMap<K, V> extends BaseUniqueMap<K, V> {
+public class FWeakValueUniqueMap<K, V> extends FUniqueMap<K, V> {
     public FWeakValueUniqueMap() {
         super(new FWeakValueMap<>(), new FWeakKeyMap<>());
     }
